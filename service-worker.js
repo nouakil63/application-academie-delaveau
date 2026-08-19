080cb09dd27d33ac25273380f46936e902fd8cbf
nouakil63 <nouakil63@users.noreply.github.com>
2026-08-19 12:02:05 +0200

    Ajouter la connexion parent liée à un élève

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "academie-delaveau-v3";
+const CACHE = "academie-delaveau-v4";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./assets/logo-academie-delaveau.png"];
 
 self.addEventListener("install", event => {
