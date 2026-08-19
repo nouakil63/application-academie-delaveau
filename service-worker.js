3c1c784f60d077c644c4ae57557c8b1f68d9e6cf
nouakil63 <nouakil63@users.noreply.github.com>
2026-08-20 01:47:23 +0200

    Épurer et aérer l’interface mobile

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "academie-delaveau-v5";
+const CACHE = "academie-delaveau-v6";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./assets/logo-academie-delaveau.png"];
 
 self.addEventListener("install", event => {
